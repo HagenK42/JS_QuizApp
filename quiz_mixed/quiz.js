0d95b9a61d180ac9d88248c533f8c3d2755881e6
HagenK42 <666.thurisaz.666@gmail.com>
2025-03-28 17:23:58 +0100

    added a new index.html and put quizzes in own folders to add a new menu and more quizzes

--- quiz_mixed/quiz.js
+++ quiz_mixed/quiz.js
@@ -14,7 +14,7 @@ let questions = [
         ],
         correctAnswerIndex: 2,
         explanation: 'Correct Answer: Hyper Text Markup Language Explanation: HTML stands for Hyper Text Markup Language. It is a standard markup language used for creating and structuring the content on web pages. HTML uses tags to define elements such as headings, paragraphs, images, links, and more. These tags provide a structure and format to the content, allowing web browsers to interpret and display the information correctly. HTML is the foundation of web development and is essential for creating and designing websites.',
-        picture: "./assets/images/code-1076536_1280.jpg",
+        picture: "../assets/images/code-1076536_1280.jpg",
     },
     {
         question: 'Does the following C++ script have errors? #include "stdafx.h" #include <iostream> using namespace std; int main() {          cout<<"Hello world"<<endl          return 0; }',
@@ -24,7 +24,7 @@ let questions = [
         ],
         correctAnswerIndex: 0,
         explanation: 'Correct Answer: Yes Explanation: The given C++ script has errors because there is a missing semicolon (;) after the cout statement.',
-        picture: "./assets/images/c-code.webp",
+        picture: "../assets/images/c-code.webp",
     },
     {
         question: 'In Java, a method is a container that holds classes.',
@@ -34,7 +34,7 @@ let questions = [
         ],
         correctAnswerIndex: 1,
         explanation: 'Correct Answer: False Explanation: In Java, a method is not a container that holds classes. A method is a block of code that performs a specific task and is defined within a class. It is used to encapsulate a set of instructions that can be called and executed when needed. Classes, on the other hand, are used to define objects and their properties and behaviors. Therefore, the statement that a method is a container that holds classes is incorrect.',
-        picture: "./assets/images/javacode.jpg",
+        picture: "../assets/images/javacode.jpg",
     },
     {
         question: '<h1>Text</h1> is the correct way of making a header in HTML.',
@@ -44,7 +44,7 @@ let questions = [
         ],
         correctAnswerIndex: 0,
         explanation: 'Correct Answer: True Explanation: In HTML, the tag is used to create a header, and it is the correct way to make a header in HTML. The tag represents the highest level of heading and is typically used for main headings on a webpage. It is important to use the appropriate HTML tags to structure and format the content correctly, and in this case, using the tag for a header is the correct approach.',
-        picture: "./assets/images/code-1076536_1280.jpg",
+        picture: "../assets/images/code-1076536_1280.jpg",
     },
     {
         question: 'Which of the following is the correct way of making a string in Java?',
@@ -56,7 +56,7 @@ let questions = [
         ],
         correctAnswerIndex: 3,
         explanation: 'Correct Answer: String text = "text"; Explanation: The correct way of making a string in Java is by using double quotation marks. Therefore, the correct answer is "String text = "text";".',
-        picture: "./assets/images/javacode.jpg",
+        picture: "../assets/images/javacode.jpg",
     },
     {
         question: 'Which of the following is the correct way to use the standard namespace in C++?',
@@ -68,7 +68,7 @@ let questions = [
         ],
         correctAnswerIndex: 0,
         explanation: 'Correct Answer: Using namespace std; Explanation: The correct way to use the standard namespace in C++ is by using the statement "using namespace std;". This statement allows you to access all the standard library functions and objects without having to specify the "std::" prefix every time.',
-        picture: "./assets/images/c-code.webp",
+        picture: "../assets/images/c-code.webp",
     },
     {
         question: 'Is this HTML code correct? <html> <head> <title>Title</title> </head> <h1>Header</h1> <p>Paragraph</p> </html>',
@@ -78,7 +78,7 @@ let questions = [
         ],
         correctAnswerIndex: 1,
         explanation: 'Correct Answer: No Explanation: The HTML code is not correct because the and elements should be placed inside the element, not directly inside the element.',
-        picture: "./assets/images/code-1076536_1280.jpg",
+        picture: "../assets/images/code-1076536_1280.jpg",
     },
     {
         question: 'Is it less bytes to use:       long l = 100; then:       int i = 10;',
@@ -88,7 +88,7 @@ let questions = [
         ],
         correctAnswerIndex: 1,
         explanation: 'Correct Answer: No Explanation: The given answer "No" is correct because both variables, long and int, occupy the same amount of memory in Java. The difference lies in the range of values they can hold. A long variable can store larger values than an int variable, but it does not necessarily use more memory. In this case, both variables are assigned small values, so they would occupy the same number of bytes.',
-        picture: "./assets/images/javacode.jpg",
+        picture: "../assets/images/javacode.jpg",
     },
     {
         question: 'Is this the correct way to make an object in Java? Class class = new Class();',
@@ -98,7 +98,7 @@ let questions = [
         ],
         correctAnswerIndex: 1,
         explanation: 'Correct Answer: False Explanation: In Java, "Class" is a reserved keyword used for reflection to obtain metadata about classes at runtime. Therefore, naming a variable "class" may lead to confusion. A correct way to instantiate an object of a class named "Class" would be: Class className = new Class();',
-        picture: "./assets/images/javacode.jpg",
+        picture: "../assets/images/javacode.jpg",
     },
     {
         question: 'Is this how you import something in C++? #include <string>',
@@ -108,7 +108,7 @@ let questions = [
         ],
         correctAnswerIndex: 0,
         explanation: 'Correct Answer: Yes Explanation: In C++, the #include directive is used to import libraries or header files, and <string> is a standard C++ header file that provides functions and classes for working with strings. So, #include <string> is used to include the functionality related to strings in your C++ program.',
-        picture: "./assets/images/c-code.webp",
+        picture: "../assets/images/c-code.webp",
     },
 ];
 
